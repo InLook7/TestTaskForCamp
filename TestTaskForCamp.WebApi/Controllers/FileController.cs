24654a7f2cc804a817ed4fafb56e0750f4aea517
InLook7 <vadym.exi@gmail.com>
2024-02-21 22:57:54 +0200

    init project

---
+++ TestTaskForCamp.WebApi/Controllers/FileController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestTaskForCamp.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class FileController : ControllerBase
+{
+    [HttpPost("uploadFile")]
+    public string UploadFile()
+    {
+        return "";
+    }
+}
